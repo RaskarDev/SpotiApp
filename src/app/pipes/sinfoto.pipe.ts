5e931904c6863973867d02e3ca55020626030366
Raskartar <alexcdev95@gmail.com>
2017-05-15 13:20:39 +0200

    Funcionalidad de busqueda

--- src/app/pipes/sinfoto.pipe.ts
+++ src/app/pipes/sinfoto.pipe.ts
@@ -5,13 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SinfotoPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any, args?: any): string {
+
+    const noImage = 'assets/img/noimage.png';
 
     if (!value) {
-      return 'assets/img/noimage.png';
+      return noImage;
     }
 
-    return null;
+    return (value.length > 0) ? value[1].url : noImage;
   }
 
 }
